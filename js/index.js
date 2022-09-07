4e10aa28516fbd42b8cc8b34285f6fa9c2c017d6
kwanele <kwaneleshelembe841@gmail.com>
2022-09-07 13:32:40 +0200

    updated navbar logic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-let links=document.querySelectorAll(".link");
+const links=document.querySelectorAll(".link");
+const portfolioNav=document.querySelector("#portfolio-nav");
 
 for (var i = 0; i < links.length; i++) {
 	links[i].onclick=(event)=>{
@@ -6,5 +7,6 @@ for (var i = 0; i < links.length; i++) {
 			links[i].classList.remove("active");
 		}
 		event.target.classList.add("active");
+		portfolioNav.classList.remove("show");
 	}
 }
