fa7243bf8f6372a159409f9635962e3d1a43d995
kwanele <kwaneleshelembe841@gmail.com>
2022-09-23 18:07:25 +0200

    made responsive transparent navbar

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ for (var i = 0; i < links.length; i++) {
 		portfolioNav.classList.remove("show");
 	}
 }
+
+
+const cover=document.querySelector("#cover");
+const navbar=document.querySelector("nav");
+
+const options={rootMargin:"-150px 0px 0px 0px"}
+
+const coverObserver=new IntersectionObserver((entries)=>{
+	if(entries[0].isIntersecting){
+		navbar.classList.remove("black-nav");
+	}else{
+		navbar.classList.add("black-nav")
+	}
+},options);
+
+coverObserver.observe(cover);
